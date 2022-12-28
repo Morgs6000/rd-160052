15543d5ce250c663f6c6897be6ff267f4b32b18f
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2022-12-28 16:48:38 -0300

    Perlin Noise, Mira e Bloco selecionado

---
+++ Assets/Scripts/World/VoxelType.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum VoxelType {
+    // Pre-Classic | rd-132211
+    air,
+    stone,
+    grass_block,
+
+    // Pre-Classic | rd-160052
+    dirt,
+    cobblestone,
+    oak_planks,
+}
